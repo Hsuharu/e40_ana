69a95b297e0189a63df79cbb0408ab6be7089cf2
S. Hoshino <suharu@km.phys.sci.osaka-u.ac.jp>
2018-06-21 05:16:46 +0900

    minor change

--- macro/hodprm/STOF1_jun.C
+++ macro/hodprm/STOF1_jun.C
@@ -264,11 +264,15 @@ void STOF1_jun( int month, int runnum){
         TOFDBG[i] = new TH1D(Form("TOFDBG%d",i+1),Form("TOFDBG%d",i+1),600,0,600);
         TOFUdE[i] = new TH1D(Form("TOFUdE%d",i+1),Form("TOFUdE%d",i+1),xbin,0,3);
         TOFDdE[i] = new TH1D(Form("TOFDdE%d",i+1),Form("TOFDdE%d",i+1),xbin,0,3);
-        SHist[i] = new TH2D(Form("SHist%d",i+1),Form("SHist%d",i+1),2000,0,2000,100,30,40);
+//        SHist[i] = new TH2D(Form("SHist%d",i+1),Form("SHist%d",i+1),2000,0,2000,100,30,40);
       }
+        SHist[0] = new TH2D(Form("SHist%d",0+1),Form("SHist%d",0+1),2000,0,2000,100,30,40);
+        SHist[1] = new TH2D(Form("SHist%d",1+1),Form("SHist%d",1+1),2000,0,2000,100,30,40);
+        SHist[2] = new TH2D(Form("SHist%d",2+1),Form("SHist%d",2+1),2000,0,2000,100,25,35);
+        SHist[3] = new TH2D(Form("SHist%d",3+1),Form("SHist%d",3+1),2000,0,2000,100,25,35);
    TH1D *TOFHitPat = new TH1D("TOFHitPat","TOFHitPat",NumOfSegTOF+1,0,NumOfSegTOF+1);
    TH1D *STOF1 = new TH1D("STOF1","STOF1",100,30,40);
-   TH1D *STOFCORR1 = new TH1D("STOFCORR1","STOFCORR1",100,30,40);
+   TH1D *STOFCORR1 = new TH1D("STOFCORR1","STOFCORR1",100,25,35);
 
    Long64_t nentries = tree->GetEntries();
    double fitprm[3];
@@ -538,10 +542,9 @@ void STOF1_jun( int month, int runnum){
 
       if(tofut[10]>0 && tofdt[10]>0 && bh2ut[3]>0 && bh2dt[3]>0 && bh2nhits  == 1){
 //      double ctofmtime = ch2ns*(tofut[10][0] + tofdt[10][0])*0.5 - (a[0][0]/sqrt(a[0][1] + tofua[10]) + a[0][2]) - (a[1][0]/sqrt(a[1][1] + tofda[10]) + a[1][2]);
-      double ctofmtime = ch2ns*(tofut[10][0] + tofdt[10][0])*0.5 - (a[0][0]/sqrt(a[0][1] + tofua[10]) ) - (a[1][0]/sqrt(a[1][1] + tofda[10])+2 );
+      double ctofmtime = ch2ns*(tofut[10][0] + tofdt[10][0])*0.5 - (a[0][0]/sqrt(a[0][1] + tofua[10]) ) - (a[1][0]/sqrt(a[1][1] + tofda[10]) );
       double bh2mtime  = ch2ns*(bh2ut[3][0] + bh2dt[3][0])*0.5;
       double stof = ctofmtime - bh2mtime ;
-        SHist[2]->Fill(tofua[10],ctofmtime - bh2mtime);   
         SHist[2]->Fill(tofua[10],ctofmtime - bh2mtime);   
         SHist[3]->Fill(tofda[10],ctofmtime - bh2mtime);   
         STOFCORR1->Fill(ctofmtime - bh2mtime);   
