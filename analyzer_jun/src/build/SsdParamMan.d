b3fd205c88a884024b43bceb266f667c704a5ba8
S. Hoshino <suharu@km.phys.sci.osaka-u.ac.jp>
2018-11-06 15:58:06 +0900

    minor change

--- analyzer_jun/src/build/SsdParamMan.d
+++
@@ -1,4 +0,0 @@
-src/build/SsdParamMan.o: src/SsdParamMan.cc include/SsdParamMan.hh \
- include/DetectorID.hh \
- /group/had/sks/software/unpacker/unpacker.gcc530/include/std_ostream.hh \
- include/DeleteUtility.hh
