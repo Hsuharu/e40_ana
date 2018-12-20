97f4ad0ad5fc2a7ac72383a79724e96686ef5ae3
S. Hoshino <suharu@km.phys.sci.osaka-u.ac.jp>
2018-12-21 06:17:16 +0900

    minor change

--- macro/trigger/SAC_Efficiency.C
+++ macro/trigger/SAC_Efficiency.C
@@ -628,15 +628,15 @@ void SAC_Efficiency(int month,int runnum){
      if(chisqrKurama[ntKurama]<chisqr &&
          qKurama[ntKurama]>0&&
          ntKurama==1 &&
-         nhSac==1&&
-         tSac[nhSac]>tSacGateMin&&
-         tSac[nhSac]<tSacGateMax&&
+         nhSac==1//&&
 //         xsacKurama[ntKurama]>xsacKuramaGateMin&&
 //         xsacKurama[ntKurama]>xsacKuramaGateMin&&
 //         ysacKurama[ntKurama]>ysacKuramaGateMax&&
 //         ysacKurama[ntKurama]>ysacKuramaGateMax&&
-         trigflag[19]>trigflag19GateMin&&
-         trigflag[19]<trigflag19GateMax
+//         tSac[nhSac]>tSacGateMin&&
+//         tSac[nhSac]<tSacGateMax&&
+//         trigflag[19]>trigflag19GateMin&&
+//         trigflag[19]<trigflag19GateMax
        ){
        Hist1[14]->Fill(tSac[nhSac]);
        Hist1[17]->Fill(xsacKurama[ntKurama]);
