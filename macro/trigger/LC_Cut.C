d610d7b456aa3eb218a32c2786e07f0b92872bc3
S. Hoshino <suharu@km.phys.sci.osaka-u.ac.jp>
2018-12-31 19:50:40 +0900

    minor change

--- macro/trigger/LC_Cut.C
+++ macro/trigger/LC_Cut.C
@@ -259,7 +259,7 @@ void LC_Cut(int month, int runnum){
   
   for(int i=0;i<NumOfSegLC;i++){
     Hist1[i]= new TH1D(Form("LC Mt Tdc %d",i+1),Form("LC Mt Tdc %d",i+1),2000,0,2000);
-    Hist1[30+i]= new TH1D(Form("LC Mt Time %d",i+1),Form("LC Mt Tdime %d",i+1),2000,0,2000);
+    Hist1[30+i]= new TH1D(Form("LC Mt Time %d",i+1),Form("LC Mt Tdime %d",i+1),1000,-500,500);
   }
   Hist1[28]= new TH1D("LC Mt Nhits","LC Mt Nhits",28,1,29);
   Hist1[29]= new TH1D("LC Mt Hitpattern","LC Mt Hitpattern",28,1,29);
