9e43ddd28fdaae2b19df299d7d457202bac4bfd8
S. Hoshino <suharu@km.phys.sci.osaka-u.ac.jp>
2019-01-02 20:27:06 +0900

    minor change

--- macro/trigger/LC_Cut.C
+++ macro/trigger/LC_Cut.C
@@ -315,6 +315,7 @@ void LC_Cut(int month, int runnum){
   //-Legend def --------------------------------------------------------------------------------------
   TLegend *Leg1 = new TLegend(0.78,0.775,0.98,0.935);
   TLegend *Leg2 = new TLegend(0.78,0.775,0.98,0.935);
+  TLegend *Leg3 = new TLegend(0.78,0.775,0.98,0.935);
 
   //-Event Loop --------------------------------------------------------------------------------------
   Long64_t nentries = khodo_lc->GetEntries();
@@ -488,6 +489,76 @@ void LC_Cut(int month, int runnum){
   Leg2->Draw();
   c1->Print(pdf);
 
+// index fanction -----------------------------------------------------------------------------------
+  double x[n], y1[n], y2[n], y3[n], y4[n], y5[n], y6[n];
+
+  for (int i=0; i<n; i++) {
+    x[i] = (i+1) * 0.02;
+//    y[i] = 10 * sin(x[i] + 0.2);
+    y1[i] = sqrt(1+(0.138/x[i])*(0.138/x[i]));
+    y2[i] = sqrt(1+(0.500/x[i])*(0.500/x[i]));
+    y3[i] = sqrt(1+(0.940/x[i])*(0.940/x[i]));
+    y4[i] = 1.1;
+    y5[i] = 1.49;
+    y6[i] = 1.05;
+  }
+
+  TGraph *g1 = new TGraph(n, x, y1);
+  TGraph *g2 = new TGraph(n, x, y2);
+  TGraph *g3 = new TGraph(n, x, y3);
+  TGraph *g4 = new TGraph(n, x, y4);
+  TGraph *g5 = new TGraph(n, x, y5);
+  TGraph *g6 = new TGraph(n, x, y6);
+  
+
+  g1->SetLineColor(2);
+  g2->SetLineColor(4);
+  g3->SetLineColor(3);
+  g4->SetLineStyle(9);
+  g5->SetLineStyle(10);
+  g6->SetLineStyle(5);
+
+  g1->GetXaxis()->SetRangeUser(0,2);
+  g2->GetXaxis()->SetRangeUser(0,2);
+  g3->GetXaxis()->SetRangeUser(0,2);
+  g4->GetXaxis()->SetRangeUser(0,2);
+  g5->GetXaxis()->SetRangeUser(0,2);
+  g6->GetXaxis()->SetRangeUser(0,2);
+
+  g1->GetYaxis()->SetRangeUser(0.98,1.6);
+  g2->GetYaxis()->SetRangeUser(0.98,1.6);
+  g3->GetYaxis()->SetRangeUser(0.98,1.6);
+  g4->GetYaxis()->SetRangeUser(0.98,1.6);
+  g5->GetYaxis()->SetRangeUser(0.98,1.6);
+  g6->GetYaxis()->SetRangeUser(0.98,1.6);
+
+  g1->SetLineWidth(4);
+  g2->SetLineWidth(4);
+  g3->SetLineWidth(4);
+  g4->SetLineWidth(3);
+  g5->SetLineWidth(3);
+  g6->SetLineWidth(3);
+
+  g4->SetTitle("Title;momentum[GeV/c];index");
+
+  gStyle->SetOptTitle(0);
+
+  g4->Draw();
+  g1->Draw("same");
+  g2->Draw("same");
+  g3->Draw("same");
+  g5->Draw("same");
+  g6->Draw("same");
+
+  Leg1->AddEntry(g1,"Pion"     ,"l");
+  Leg1->AddEntry(g2,"Kaon"     ,"l");
+  Leg1->AddEntry(g3,"Proton"   ,"l");
+  Leg1->AddEntry(g4,"SAC index 1.1","l");
+  Leg1->AddEntry(g5,"LC index 1.49","l");
+  Leg1->AddEntry(g6,"LAC index 1.05","l");
+  Leg1->Draw();
+
+  c1->Print("./index_plot.pdf");
 
   c1->Print(pdf+"]"); 
 
