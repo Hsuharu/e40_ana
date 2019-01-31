2cffc848915224abf9aef542d6c66cb907af4c48
S. Hoshino <suharu@km.phys.sci.osaka-u.ac.jp>
2019-02-01 01:45:31 +0900

    minor change

--- macro/trigger/Mtx_Banch.C
+++ macro/trigger/Mtx_Banch.C
@@ -908,7 +908,9 @@ void Mtx_Banch(int month, int runnum){
 
   g5->Draw("AP");
   g4->SetMarkerColor(2);
-  g4->Draw("same AP");
+  g4->SetMarkerStyle(8);
+  g4->SetMarkerSize(2);
+  g4->Draw("same");
 
   c1->Print(pdf);
   c1->Print(Form("%s/Mtx_Banch_run%05d_Graph_MtxFlg_OldAndNewPat_Accept.pdf",pdfDhire.Data(),runnum));
